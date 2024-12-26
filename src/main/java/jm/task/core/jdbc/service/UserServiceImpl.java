2024dbdf010a22ae9cce4e55ca0da2d6c51b8dde
DirectorFSB <churikov.sergei@icloud.com>
2024-12-26 16:59:04 +0400

    mod

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDaoJDBCImpl usJDBC = new UserDaoJDBCImpl();
+    private final UserDao usJDBC = new UserDaoJDBCImpl();
     public void createUsersTable() {
         usJDBC.createUsersTable();
     }
